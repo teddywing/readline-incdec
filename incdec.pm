c16d56b927ad786f8fa299b9ce5b746f7434a2e7
Teddy Wing <git@teddywing.com>
2021-08-19 23:48:57 +0200

    incdec: Comment out test code from backward match fix
    Commenting this out instead of removing it for the moment while I add
    test cases in case I need to keep debugging.

--- incdec.pm
+++ incdec.pm
@@ -10,29 +10,29 @@ sub incdec {
 	$is_backward ||= 0;
 
 	my $start_position = 0;
-	my @match_ranges;
+	# my @match_ranges;
 	my $previous_match_start = 0;
-	my $previous_match_end = 0;
+	# my $previous_match_end = 0;
 	while ($line =~ /(\d+)/g) {
 		if ($is_backward) {
-			print "p[$point_position] -[$-[0]] +[$+[0]]\n";
-			print "p[$point_position] -[$previous_match_start] +[$previous_match_end]\n";
-			print "last match: $^N\n";
-			print $previous_match_end - 1 . " <= $point_position < $-[0]\n";
+			# print "p[$point_position] -[$-[0]] +[$+[0]]\n";
+			# print "p[$point_position] -[$previous_match_start] +[$previous_match_end]\n";
+			# print "last match: $^N\n";
+			# print $previous_match_end - 1 . " <= $point_position < $-[0]\n";
 			# if ($previous_match_end - 1 <= $point_position
 			# 		&& $point_position < $-[0]) {
 			if ($point_position < $-[0]) {
-				print "match at [$previous_match_start]";
+				# print "match at [$previous_match_start]";
 				$start_position = $previous_match_start;
 
 				last;
 			}
 
 			$previous_match_start = $-[0];
-			$previous_match_end = $+[0];
+			# $previous_match_end = $+[0];
 
-			my @range = ($-[0], $+[0]);
-			push @match_ranges, \@range;
+			# my @range = ($-[0], $+[0]);
+			# push @match_ranges, \@range;
 		}
 		else {
 			if ($point_position < $+[0]) {
@@ -42,16 +42,6 @@ sub incdec {
 			}
 		}
 	}
-	print "\n,$start_position\n";
-
-	# if ($is_backward) {
-	# 	for (my $i = $#match_ranges; $i >= 0; $i--) {
-	# 		print "\n$match_ranges[$i][0]\n";
-	# 		if ($match_ranges[$i][0] <= $point_position) {
-	# 			$start_position = $match_ranges[$i][0];
-	# 		}
-	# 	}
-	# }
 
 	if ($is_backward && $point_position == length $line) {
 		$start_position = $previous_match_start;
