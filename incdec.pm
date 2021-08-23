6fb6965372d71676d3d8246204f82bd91391123a
Teddy Wing <git@teddywing.com>
2021-08-23 20:56:26 +0200

    incdec.pm: Remove recent debug prints
    Remove the debug prints when I was trying to work out the problem with
    negative numbers and the last number in the line.

--- incdec.pm
+++ incdec.pm
@@ -10,21 +10,9 @@ sub incdec {
 	$is_backward ||= 0;
 
 	my $start_position = 0;
-	# my @match_ranges;
 	my $previous_match_start = 0;
-	my $previous_match_end = 0;
 	while ($line =~ /(-?\d+)/g) {
 		if ($is_backward) {
-			print "p[$point_position] -[$-[0]] +[$+[0]]\n";
-			print "p[$point_position] -[$previous_match_start] +[$previous_match_end]\n";
-			# print "last match: $^N\n";
-			print $previous_match_end - 1 . " <= $point_position < $-[0]\n";
-			# if ($previous_match_end - 1 <= $point_position
-			# 		&& $point_position < $-[0]) {
-
-			# Try always setting $start_position and resetting it if there's another number?
-			# TODO: document, last number handling
-
 			# Set start position to the current match start. This gives us the
 			# correct start position when incrementing the last number in a
 			# line.
@@ -33,17 +21,12 @@ sub incdec {
 			# If point is not at the end, set start position to the number
 			# closest to the point position.
 			if ($point_position < $-[0]) {
-				# print "match at [$previous_match_start]";
 				$start_position = $previous_match_start;
 
 				last;
 			}
 
 			$previous_match_start = $-[0];
-			$previous_match_end = $+[0];
-
-			# my @range = ($-[0], $+[0]);
-			# push @match_ranges, \@range;
 		}
 		else {
 			if ($point_position < $+[0]) {
